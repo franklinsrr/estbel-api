9533b59300cc5fc0cfd1db1db4edf11a77f32552
Franklin Rodriguez <franklinserif@gmail.com>
2025-06-25 17:10:37 -0400

    feat(stats): add endpoint to retrieve members with birthdays today

--- src/stats/stats.controller.ts
+++ src/stats/stats.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { StatsService } from './stats.service';
 
 @Controller('stats')
 export class StatsController {
   constructor(private readonly statsService: StatsService) {}
+
+  /**
+   * Get all members with birthday today
+   * @returns Array of members with birthday today
+   */
+  @Get('birthday/:month/:day')
+  async getBirthdayMembers(
+    @Param('month') month: number,
+    @Param('day') day: number,
+  ) {
+    const members = await this.statsService.getBirthdayMembers(month, day);
+
+    return members;
+  }
 }
