d665251a3650716c94fe3e6b0a73daddba38e6dc
Franklin Rodriguez <franklinserif@gmail.com>
2025-06-25 17:10:33 -0400

    feat(stats): import MembersModule to enhance stats functionality

--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StatsService } from './stats.service';
 import { StatsController } from './stats.controller';
+import { MembersModule } from '@members/members.module';
 
 @Module({
   controllers: [StatsController],
   providers: [StatsService],
+  imports: [MembersModule],
 })
 export class StatsModule {}
