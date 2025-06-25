bd1d9acd04e9c1e02c0680b2b0f6334e622d6e85
Franklin Rodriguez <franklinserif@gmail.com>
2025-06-25 19:07:42 -0400

    feat(stats): enhance StatsModule by adding AuthModule and AdminsModule imports

--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { StatsService } from './stats.service';
 import { StatsController } from './stats.controller';
 import { MembersModule } from '@members/members.module';
+import { AdminsModule } from '@admins/admins.module';
+import { AuthModule } from '@auth/auth.module';
 
 @Module({
   controllers: [StatsController],
   providers: [StatsService],
-  imports: [MembersModule],
+  imports: [
+    MembersModule,
+    forwardRef(() => AuthModule),
+    forwardRef(() => AdminsModule),
+    AuthModule,
+  ],
 })
 export class StatsModule {}
